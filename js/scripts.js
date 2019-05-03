f4fe5379edabc49cd1fdee692804a68160ca1cb6
Francis Mukuha <mukuha58@gmail.com>
2019-05-03 15:22:24 +0300

    modified js file for what we do section to hide and show upon click

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function () {
+  $(".designhidden, .designshowing").click(function () {
+    $(".designshowing").toggle();
+    $(".designhidden").toggle();
+  });
+  $(".devhidden, .devshowing").click(function () {
+    $(".devshowing").toggle();
+    $(".devhidden").toggle();
+  });
+  $(".producthidden, .productshowing").click(function () {
+    $(".productshowing").toggle();
+    $(".producthidden").toggle();
+  });
